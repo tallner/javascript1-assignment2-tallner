f8f2c7863036d2765f1c4e7adbb142cf9f238768
tallner <ctallner@hotmail.com>
2022-01-15 13:42:28 +0100

    comit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
-const 
+//location.reload()
+
+const playerName = document.querySelector('.player-name');
+playerName.addEventListener('keypress', pressEnter);
+playerName.addEventListener('click',e => playerName.value='');
+playerName.
+
+playerName.addEventListener('change', function () {
+
+    // if(!playerName.value){
+    //     playerName.value='add name';
+    //     console.log('name empty: ', playerName.value);
+    // }
+
+    console.log('value is changed to: ', playerName.value);
+
+  });
+
+function pressEnter(e){
+    if(e.key === 'Enter'){
+        //playerName.value = 'hej';
+        playerName.style.textAlign = 'center';
+        playerName.style.border = 'white solid 0px';
+        playerName.blur();
+    }
+}
+
+
+const btnTryAgain = document.querySelector('.footer > button');
+btnTryAgain.addEventListener('click', tryAgain);
+function tryAgain(e){
+    //reset everyting to default
+    playerName.value = 'enter your name';
+}
